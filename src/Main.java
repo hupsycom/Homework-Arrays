e188188a6e6b1b7f4a4bf2b5c64626676ff40b77
origin <dreamchillwithme@gmail.com>
2024-03-15 12:52:42 +0300

    Сделано ДЗ Массивы ч.1

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,29 @@ public class Main {
 
         System.out.println("Задача №1");
 
-        int[] arr1 = new int[]{1, 2, 3};
+        int[] arr1 = new int[3];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
         double[] arr2 = new double[]{1.57, 7.654, 9.986};
         int[] arr3 = {1, 2, 3, 4, 5};
 
         System.out.println("Задача №2");
 
-        System.out.println(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(arr2));
-        System.out.println(Arrays.toString(arr3));
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + ", ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + ", ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i] + ", ");
+        }
+        System.out.println();
 
         System.out.println("Задача №3");
 
